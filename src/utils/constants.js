5d1acc32f75ca5ed54a237bb332d9af74b4555f7
Ruchik <ruchikjoshi1998@gmail.com>
2025-03-05 18:17:13 +0100

    reframing the environment variables

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,8 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer '+ process.env.REACT_APP_TBDM_AUTH
+      //Authorization: 'Bearer '+ process.env.REACT_APP_TBDM_AUTH
+      Authorization: 'Bearer '+ import.meta.env.VITE_REACT_APP_TBDM_AUTH
     }
   };
 
@@ -19,4 +20,5 @@ export const API_OPTIONS = {
     {id: "hindi", name: "Hindi"}
   ];
 
-  export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+  //export const OPENAI_KEY = process.env.REACT_APP_OPENAI_KEY;
+  export const OPENAI_KEY = import.meta.env.VITE_REACT_APP_OPENAI_KEY;
